4f69776a7559090d6b8dbfd185eb2cc59b00d38a
Alexy <alexy@Renauds-iMac.local>
2017-11-03 15:52:32 +0100

    add gray indicator in top bar + back press

--- FrenchBlues/ViewController.swift
+++ FrenchBlues/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+    
 
+
+
+
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    var webView: WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        self.webView = WKWebView()
+        self.view = self.webView
+        self.webView.navigationDelegate = self
+        webView.bringSubview(toFront: indicator)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let myURL = URL(string: "http://www.frenchblues.fr")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func backButton(_ sender: Any) {
+        webView.goBack()
+    }
 
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        indicator.startAnimating()
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        indicator.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+    }
 
 }
 
